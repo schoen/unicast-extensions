a33a0dd6a137ba78ba6d1ed0514d96eea93d9061
Seth Schoen <schoen@loyalty.org>
2020-10-12 11:13:30 -0700

    Update status of TODOs

--- tests/linux-selftest.sh
+++ tests/linux-selftest.sh
@@ -11,13 +11,12 @@
 # This test must be run as root (to manipulate the network namespaces
 # and virtual interfaces).
 
-# TODO: This needs to be updated to better match the coding style of
-#       existing network tests.
-
 # TODO: This needs a nettest binary, from linux/tools/testing/selftests/net;
 #       either integrate this with the selftests so this is guaranteed to
 #       be available, or use a different dependency for TCP socket testing!
 
+# TODO: can we make this faster by having nettest indicate exactly
+#       when the listening socket has been bound or something?
 
 if [ -x ./nettest ]; then
 	NETTEST=./nettest
@@ -52,8 +51,6 @@ ip netns exec foo-ns timeout 2 ping -c 1 $2 || return 1
 ip netns exec bar-ns timeout 2 ping -c 1 $1 || return 1
 
 # using nettest (for this simple test, it's akin to netcat)
-# (can we make this faster by having nettest indicate exactly
-# when the listening socket has been bound or something?)
 ip netns exec foo-ns "$NETTEST" -s &
 sleep 0.5
 ip netns exec bar-ns "$NETTEST" -r $1 || return 1
